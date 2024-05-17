4066d451b02aa43e151f6d12d8614c2b746e9498
YanyaGG <147350470+YanyaGG@users.noreply.github.com>
2024-05-17 12:52:57 +0100

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+
+
+var A = "Hi, this is my alert!"; //This is assigning a variable A
+window.alert(A) //This is the alert box
+
+var Sent1 = "This is the beginning of the string"; //Assigning variable Sent1
+var Sent2 = " and this is the end of the string"; //Assigning variable Sent2
+document.write(Sent1 + Sent2) //Concatenating variables Sent1 and Sent2 together, this also contains an expression Sent1 + Sent2
+
+
+
