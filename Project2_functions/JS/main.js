5c25b49e77cb68f65d0f343438f4a60fa74be4d8
YanyaGG <147350470+YanyaGG@users.noreply.github.com>
2024-05-18 12:57:53 +0100

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+//This is a function that uses the =+ operator
+function My_First_Function () {
+    var str = "This is the button text!"; //creating the string variable
+    str += " This is more button text."; //using += operator
+    document.getElementById("Button_Text").innerHTML = str; //using document.getElementById()
+}
+
