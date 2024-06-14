de4da71819bcfe6f8b25e24a7bcb413c2060aa27
YanyaGG <147350470+YanyaGG@users.noreply.github.com>
2024-06-14 18:37:37 +0100

    Update main.js

--- Project6_ternary_operators_constructors/JS/main.js
+++ Project6_ternary_operators_constructors/JS/main.js
@@ -1,10 +1,11 @@
-function Vote_Funtion() {
+//Function using a ternary operation
+function Vote_Function() {
     var Age = document.getElementById("Age").value;
     var Can_vote = (Age >= 18) ? "You are old enough " : "You are too young ";
     document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
 }
 
-
+//Constructor function using "this" and "new" keywords
 function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
@@ -12,6 +13,7 @@ function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Color = Color;
 }
 
+//Function to display the results of the constructor 
 var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
@@ -20,6 +22,7 @@ function myFunction() {
     document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
 }
 
+//A nested function
 function add_Strings() {
     var start_string = "Hello";
 
